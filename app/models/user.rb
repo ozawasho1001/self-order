4392b313a74cb3159c301e0d178990cf3145535d
”ozawasho1001” <ozawasho10@gmail.com>
2020-12-08 07:20:56 +0000

    新規登録メールアドレスのバリデーションの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates :password, length: { in: 1..10 }
   has_secure_password
 end
